4c01d1dd8dd76b67a5a1ecfab25348e0db1eb07f
cj <864099641@qq.com>
2021-05-08 11:08:12 +0800

    tip:BaseActivity、BaseFragment完善

--- Project_base/base/src/main/java/com/cj/base/activity/BaseActivity.kt
+++ Project_base/base/src/main/java/com/cj/base/activity/BaseActivity.kt
@@ -1,5 +1,12 @@
 package com.cj.base.activity
 
+import android.graphics.Rect
+import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
 
 /**
@@ -8,5 +15,81 @@ import androidx.appcompat.app.AppCompatActivity
  * @author CJ
  * @date 2021/5/6 19:56
  */
-class BaseActivity : AppCompatActivity() {
+abstract class BaseActivity : AppCompatActivity() {
+
+    /**
+     * 获取布局ID
+     */
+    @LayoutRes
+    abstract fun getLayoutId(): Int
+
+    /**
+     * 初始化数据
+     */
+    abstract fun initData(bundle: Bundle?)
+
+    /**
+     * 初始化View
+     */
+    abstract fun initView()
+
+    /**
+     * 设置监听
+     */
+    abstract fun initListener()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setRootView(getLayoutId())
+        initData(savedInstanceState)
+        initView()
+        initListener()
+    }
+
+    override fun dispatchTouchEvent(event: MotionEvent?): Boolean {
+        if (MotionEvent.ACTION_UP == event?.action) {
+            currentFocus?.let {
+                if (isSoftShowing() && isShouldHideInput(it, event)) {
+                    val inputMethodManager =
+                        getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+                    inputMethodManager.hideSoftInputFromWindow(it.windowToken, 0)
+                }
+            }
+
+        }
+        return super.dispatchTouchEvent(event)
+    }
+
+    protected fun setRootView(@LayoutRes layoutId:Int){
+        setContentView(layoutId)
+    }
+
+    //软键盘是否显示
+    private fun isSoftShowing(): Boolean {
+        //获取当前屏幕内容的高度
+        val screenHeight = window.decorView.height
+        //获取View可见区域的bottom
+        val rect = Rect()
+        window.decorView.getWindowVisibleDisplayFrame(rect)
+
+        return screenHeight - rect.bottom != 0
+    }
+
+    //是否需要隐藏键盘
+    private fun isShouldHideInput(view: View, event: MotionEvent): Boolean {
+        if (view is EditText) {
+            val leftTop = intArrayOf(0, 0)
+            //获取输入框当前的location位置
+            view.getLocationInWindow(leftTop)
+            val left = leftTop[0]
+            val top = leftTop[1]
+            val bottom = top + view.getHeight()
+            val right = left + view.getWidth()
+            // 点击的是输入框区域，保留点击EditText的事件
+            return (event.x <= left || event.x >= right
+                    || event.y <= top || event.y >= bottom)
+        }
+        return false
+    }
+
 }
--- Project_base/base/src/main/java/com/cj/base/fragment/BaseFragment.kt
+++ Project_base/base/src/main/java/com/cj/base/fragment/BaseFragment.kt
@@ -1,6 +1,13 @@
 package com.cj.base.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.annotation.LayoutRes
 import androidx.fragment.app.Fragment
+import com.cj.framework.utils.getField
 
 /**
  * BaseFragment
@@ -8,5 +15,89 @@ import androidx.fragment.app.Fragment
  * @author CJ
  * @date 2021/5/6 19:57
  */
-class BaseFragment : Fragment() {
+abstract class BaseFragment : Fragment() {
+
+    /**
+     * 获取布局ID
+     */
+    @LayoutRes
+    abstract fun getLayoutId(): Int
+
+    /**
+     * 初始化数据
+     */
+    abstract fun initData(bundle: Bundle?)
+
+    /**
+     * 初始化View
+     */
+    abstract fun initView()
+
+    /**
+     * 设置监听
+     */
+    abstract fun initListener()
+
+    private val mBackPressedCallback = object : OnBackPressedCallback(false) {
+        override fun handleOnBackPressed() {
+            onBackPressed()
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requireActivity().onBackPressedDispatcher.addCallback(mBackPressedCallback)
+        initData(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflateLayout(inflater, container, getLayoutId())
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+        initListener()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mBackPressedCallback.remove()
+    }
+
+    protected fun inflateLayout(
+        inflater: LayoutInflater,
+        container: ViewGroup?, @LayoutRes layoutId: Int
+    ): View {
+        return inflater.inflate(layoutId, container, false)
+    }
+
+    /**
+     * 设置back按键处理事件
+     */
+    protected open fun setBackEnabled(enabled: Boolean) {
+        mBackPressedCallback.isEnabled = enabled
+    }
+
+    /**
+     * 返回键点击处理
+     */
+    protected fun onBackPressed() {
+        //do nothing 子类重写
+    }
+
+    /**
+     * 返回
+     */
+    protected fun back() {
+        getField<Runnable>(
+            requireActivity().onBackPressedDispatcher,
+            "mFallbackOnBackPressed"
+        )?.run()
+    }
+
 }
